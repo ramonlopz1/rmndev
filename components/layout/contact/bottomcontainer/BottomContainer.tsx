a3535279584a1bebc4e6987a1c69bda5201b1e41
ramonlopz1 <davidramon123@hotmail.com>
2022-11-21 20:41:55 -0300

    email component

--- components/layout/contact/bottomcontainer/BottomContainer.tsx
+++ components/layout/contact/bottomcontainer/BottomContainer.tsx
@@ -3,7 +3,18 @@ import styles from './BottomContainer.module.css'
 export default function BottomContainer(): JSX.Element {
     return (
         <div className={styles.bottom_container}>
+            <div className={styles.sub_container}>
+                <h3>Contato</h3>
+                <div className={styles.contact_info}>
 
+                </div>
+            </div>
+            <div className={styles.sub_container}>
+                <h3>Redes</h3>
+                <div className={styles.web_info}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
--- components/layout/contact/topcontainer/TopContainer.tsx
+++ components/layout/contact/topcontainer/TopContainer.tsx
@@ -1,10 +1,30 @@
+import { useState } from 'react'
 import styles from './TopContainer.module.css'
 
 export default function TopContainer(): JSX.Element {
+
+    const [emailContent, setEmailContent] = useState({})
+
+    const emailHandler = (event) => {
+        event.preventDefault()
+
+        const email = {
+            name: event.target[0].value,
+            email: event.target[1].value,
+            subject: event.target[2].value,
+            textarea: event.target[3].value,
+        }
+
+        setEmailContent(email)
+    }
+
     return (
         <div className={styles.top_container}>
-            <h3 className={styles.title}>Contato</h3>
-            <div className={styles.email_information}>
+            <h3 className={styles.title}>Enviar mensagem</h3>
+            <form
+                onSubmit={e => emailHandler(e)}
+                className={styles.email_information}
+            >
                 <div className={styles.inputs}>
                     <div className={styles.email_input}>
                         <label htmlFor="name" className={styles.email_label}>Nome</label>
@@ -12,14 +32,16 @@ export default function TopContainer(): JSX.Element {
                             type="text"
                             name="name"
                             placeholder='Insira o seu nome'
+                            required
                         />
                     </div>
                     <div className={styles.email_input}>
-                        <label htmlFor="email" className={styles.email_label}>Email</label>
+                        <label htmlFor="email" className={styles.email_label}>E-mail</label>
                         <input
                             type="text"
                             name="email"
                             placeholder='Insira o seu email'
+                            required
                         />
                     </div>
                     <div className={styles.email_input}>
@@ -28,16 +50,31 @@ export default function TopContainer(): JSX.Element {
                             type="text"
                             name="subject"
                             placeholder='Insira o assunto'
+                            required
                         />
                     </div>
                 </div>
                 <div className={styles.email_content}>
+                    <h4 className={styles.content_title}>Mensagem</h4>
                     <textarea
                         name="content"
                         placeholder='Insira o corpo do e-mail'
                     ></textarea>
+                    <div className={styles.email_btns}>
+                        <div className={styles.email_terms}>
+                            <label htmlFor="accept_terms">Aceito os termos de uso: </label>
+                            <input
+                                type="checkbox" name="accept_terms"
+                                className={styles.checkbox}
+                                required
+                            />
+                        </div>
+
+                        <input type="submit" value="Enviar" className={styles.email_btn_send} />
+
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
